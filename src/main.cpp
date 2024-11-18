09bcd3e7f3bd73b1231272a0fd19446f35aee33f
Emilie Gonin <emilie.yuu@gmail.com>
2024-11-18 11:00:30 +0100

    add sdl

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+#define SDL_MAIN_HANDLED
+
+#include <iostream>
+#include <SDL.h>
+
 #include "raylib.h"
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
@@ -41,9 +46,62 @@ static void RayLibExample()
 	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
 }
+static int SDLExample()
+{
+	// Pointers to our window and surface
+	SDL_Surface* winSurface = NULL;
+	SDL_Window* window = NULL;
+
+	// Initialize SDL. SDL_Init will return -1 if it fails.
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		std::cout << "Error initializing SDL: " << SDL_GetError() << std::endl;
+		system("pause");
+		// End the program
+		return 1;
+	}
+
+	// Create our window
+	window = SDL_CreateWindow("Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN);
+
+	// Make sure creating the window succeeded
+	if (!window) {
+		std::cout << "Error creating window: " << SDL_GetError() << std::endl;
+		system("pause");
+		// End the program
+		return 1;
+	}
+
+	// Get the surface from the window
+	winSurface = SDL_GetWindowSurface(window);
+
+	// Make sure getting the surface succeeded
+	if (!winSurface) {
+		std::cout << "Error getting surface: " << SDL_GetError() << std::endl;
+		system("pause");
+		// End the program
+		return 1;
+	}
+
+	// Fill the window with a white rectangle
+	SDL_FillRect(winSurface, NULL, SDL_MapRGB(winSurface->format, 255, 0, 0));
+
+	// Update the window display
+	SDL_UpdateWindowSurface(window);
+
+	// Wait
+	system("pause");
+
+	// Destroy the window. This will also destroy the surface
+	SDL_DestroyWindow(window);
+
+	// Quit SDL
+	SDL_Quit();
+	return 0;
+}
 
 int main()
 {
-	RayLibExample();
+	//RayLibExample();
+	//return SDLExample();
 	return 0;
 }
